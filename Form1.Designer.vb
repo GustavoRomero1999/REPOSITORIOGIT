160110b5d091996461435f1377e589b518b3b51f
GustavoRomero <gusta.romero.gr@gmail.com>
2024-11-11 16:45:53 -0300

    AGREGO BOTON

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.BtnAceptar = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'BtnAceptar
@@ -34,18 +35,28 @@ Partial Class Form1
         Me.BtnAceptar.Text = "ACEPTAR"
         Me.BtnAceptar.UseVisualStyleBackColor = True
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(117, 61)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(249, 20)
+        Me.TextBox1.TabIndex = 1
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(501, 363)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.BtnAceptar)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents BtnAceptar As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
 
+    Private Sub BtnAceptar_Click(sender As Object, e As EventArgs) Handles BtnAceptar.Click
+        TextBox1.Text = "REPOSITORIO GIT"
+    End Sub
 End Class
